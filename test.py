3148cbd0a652afc657e74c64c03962f4cf788e4f
Shrikant8705 <shrikantshedge305@gmail.com>
2026-01-27 17:05:19 +0530

    Requests library

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
-#Basic script 
+import requests
+
+#Function to fetch data
+def fetch_mumbai():
+    url = "https://api.open-meteo.com/v1/forecast?latitude=19.076&longitude=72.877&current=relative_humidity_2m,precipitation"
+
+    response = requests.get(url)
+    return response.json()
+#Function for checking risk
 def check_weather(rain,humidity):
     if rain > 30 and humidity > 65:
         print("WARNING: High risk of a flash flood or a Cloudburst!")
     else:
         print("Weather seems to be clear!")
 
+weather=fetch_mumbai()
+rain = weather["current"]["precipitation"]
+humidity = weather["current"]["relative_humidity_2m"]
 
-usr_rainfall=int(input("Please enter the rainfall in your area:\n"))
-usr_humidity=int(input("Please enter humidity in your area:\n"))
-
-status = check_weather(usr_rainfall,usr_humidity)
-print(status)
+print(f"Current rain in Mumbai is: {rain}mm")
+print(f"Current humidity in Mumbai is:{humidity}%")
